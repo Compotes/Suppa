f22e82acb61302dcb3a0ee5c0af7b27a6c9ecc3c
Compotes <compotesrobots@gmail.com>
2015-01-12 21:20:41 +0100

    Little updates.

--- example/example.ino
+++ example/example.ino
@@ -1,14 +1,14 @@
 #include <Nemt.h>
 
-Nemt n(3); // we set number of color 1-7 // we set counter 
+Nemt n(50); // timer  ( 50 = 1s+- )
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(n.giveX()); // return type = int; x point
-  n.giveRight(); // return type = int; right point
-  n.giveLeft(); // return type = int; left point
+  n.giveX(3); // return type int // arg1 is color in this case it's yellow
+  n.giveRight(1); // color = red
+  n.giveLeft(1); // color = red
 }
 
